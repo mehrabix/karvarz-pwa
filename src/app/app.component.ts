9683dfdf7fa3531c113bab4e0350b0873b9f140a
mehrabix <mehrabi@post.com>
2025-02-22 17:53:07 +0330

    install pwa config and config a form using reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
+import { ButtonModule } from 'primeng/button';
+
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`
+  imports: [RouterOutlet,DialogModule,ButtonModule,FormsModule],
+  template: `<router-outlet></router-outlet>
+  <p-dialog header="Edit Profile" [modal]="true" [(visible)]="visibleUpdate" [style]="{ width: '25rem' }">
+    <span class="p-text-secondary block mb-8">بروزرسانی برنامه.</span>
+   آپدیت جدید در دسترس است! آیا مایل به بروزرسانی هستید؟
+    <div class="flex justify-end gap-2">
+    <p-button label="خیر" severity="secondary" (click)="visibleUpdate = false" />
+
+        <p-button label="بله" (click)="handleUpdateApp()" />
+    </div>
+</p-dialog>
+  `,
+
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  appUpdate = inject(SwUpdate)
+  visibleUpdate:boolean = false
   title = 'router-practice';
+
+
+  async ngOnInit(): Promise<void> {
+    const hasupdate =  await this.appUpdate.checkForUpdate();
+    if(hasupdate){
+      this.handleUpdateApp();
+    }
+  }
+
+
+  handleUpdateApp(){
+    document.location.reload();
+
+  }
+
+
+
 }
--- src/app/pages/home/main/main.component.ts
+++ src/app/pages/home/main/main.component.ts
@@ -1,22 +1,45 @@
 import { Location } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-
+import { FormBuilder, ReactiveFormsModule,Validators } from '@angular/forms'; 
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
 @Component({
     selector: 'selector-name',
     templateUrl: 'main.component.html',
-    imports:[ButtonModule]
+    styleUrl:`main.component.scss`,
+    imports:[ButtonModule,ReactiveFormsModule,
+        InputTextModule, 
+        ButtonModule, 
+        CardModule 
+    ]
 })
 
 export class MainPageComponent implements OnInit {
-    location = inject(Location)
-    constructor() { }
 
+    location = inject(Location)
+    userForm: any
+
+     constructor(private fb: FormBuilder) {
+        this.userForm = this.fb.group({
+            firstName: ['', Validators.required], 
+            lastName: [''],
+    
+         })      
+     }
+ 
     ngOnInit() { }
 
 
     forward(){
         this.location.forward()
+
+    }
+
+
+    getFormData(){
+        debugger
+        console.log(this.userForm.value);
 
     }
 }
